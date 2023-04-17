5dec27aedfc1910aa9f38bb3eb93f5e28fa2c7bf
Sebastian Sanchez <sebastian@vatom.com>
2023-04-17 15:37:10 -0600

    add inputs for testing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import './App.css';
 import { VatomWallet } from "@vatom/wallet-sdk"
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 
 function App() {
   const divRef = useRef(null)
 
-  const accessToken = ""
-  const businessId = ""
+  const [accessToken,setAccessToken] = useState("")
+  const [businessId,setBusinessId] = useState("")
+
+  const [shouldRender, setShouldRender] = useState(false) 
 
   useEffect(() => {
-    if(divRef.current){
-      new VatomWallet(divRef.current, accessToken, businessId)
+    if(divRef.current && shouldRender){
+      new VatomWallet(divRef.current, accessToken, businessId,{baseUrl:"http://wallet.localhost"})
     }
-  }, [divRef])
+  }, [shouldRender])
 
   return (
-    <div className="App" ref={divRef}>
+    <div>
+      {!shouldRender?
+      <>
+      <input type="text" value={accessToken} placeholder='access token' onChange={(e)=>setAccessToken(e.target.value)}/>
+      <input type="text" value={businessId} placeholder='business id' onChange={(e)=>setBusinessId(e.target.value)}/>
+      <button onClick={()=>setShouldRender(true)}>Render!</button>
+      </>
+      :
+      <div className="App" ref={divRef}>
+      </div>
+        }
 
     </div>
   );
