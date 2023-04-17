531b5d4efff4494531b4556d71e21609651c0c2b
Sebastian Sanchez <sebastian@vatom.com>
2023-04-17 12:42:08 -0600

    hello world!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { VatomWallet } from "@vatom/wallet-sdk"
+import { useEffect, useRef } from 'react';
+
 
 function App() {
+  const divRef = useRef(null)
+
+  const accessToken = ""
+  const businessId = ""
+
+  useEffect(() => {
+    if(divRef.current){
+      new VatomWallet(divRef.current, accessToken, businessId)
+    }
+  }, [divRef])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" ref={divRef}>
+
     </div>
   );
 }
