033a6cfcbb25647b32f9cded0abd3625360bba76
Luis Palacios <lpalacios.dev@gmail.com>
2023-04-20 17:38:21 -0300

    fix: defaults

--- web/src/App.js
+++ web/src/App.js
@@ -1,35 +1,41 @@
-import './App.css';
-import { VatomWallet } from "@vatom/wallet-sdk"
-import { useEffect, useRef, useState } from 'react';
-
+import "./App.css";
+import { VatomWallet } from "@vatom/wallet-sdk";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
-  const divRef = useRef(null)
+  const divRef = useRef(null);
 
-  const [accessToken,setAccessToken] = useState("")
-  const [businessId,setBusinessId] = useState("")
+  const [accessToken, setAccessToken] = useState("");
+  const [businessId, setBusinessId] = useState("");
 
-  const [shouldRender, setShouldRender] = useState(false) 
+  const [shouldRender, setShouldRender] = useState(false);
 
   useEffect(() => {
-    if(divRef.current && shouldRender){
-      new VatomWallet(divRef.current, accessToken, businessId,{baseUrl:"http://wallet.localhost"})
+    if (divRef.current && shouldRender) {
+      new VatomWallet(divRef.current, accessToken, businessId);
     }
-  }, [shouldRender])
+  }, [shouldRender, accessToken, businessId]);
 
   return (
     <div>
-      {!shouldRender?
-      <>
-      <input type="text" value={accessToken} placeholder='access token' onChange={(e)=>setAccessToken(e.target.value)}/>
-      <input type="text" value={businessId} placeholder='business id' onChange={(e)=>setBusinessId(e.target.value)}/>
-      <button onClick={()=>setShouldRender(true)}>Render!</button>
-      </>
-      :
-      <div className="App" ref={divRef}>
-      </div>
-        }
-
+      {!shouldRender ? (
+        <>
+          <input
+            type="text"
+            value={accessToken}
+            placeholder="access token"
+            onChange={(e) => setAccessToken(e.target.value)}
+          />
+          <input
+            type="text"
+            value={businessId}
+            placeholder="business id"
+            onChange={(e) => setBusinessId(e.target.value)}
+          />
+          <button onClick={() => setShouldRender(true)}>Render!</button>
+        </>
+      ) : null}
+      <div ref={divRef} className="App"></div>
     </div>
   );
 }
