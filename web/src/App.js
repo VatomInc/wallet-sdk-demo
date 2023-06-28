6d234d51ec3360ce7f11d1a0ddcefb128433927e
kdlcruz <kj_delacruz28@yahoo.com>
2023-06-29 04:33:52 +0800

    feat: identity sdk and new wallet sdk

--- web/src/App.js
+++ web/src/App.js
@@ -1,50 +1,44 @@
-import "./App.css";
-import { VatomWallet } from "@vatom/wallet-sdk";
-import { useEffect, useRef, useState } from "react";
+import './App.css';
+import { VatomIdentitySDK } from "@vatom/identity-sdk"
+import { VatomWallet } from "@vatom/wallet-sdk"
+
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
-  const divRef = useRef(null);
-
-  const [accessToken, setAccessToken] = useState("");
-  const [businessId, setBusinessId] = useState("");
-
-  const [shouldRender, setShouldRender] = useState(false);
+  const divRef = useRef(null)
+  const clientId = "94JHkdj8jF83jfFF2LI8Q4"
+  const identitySdk = new VatomIdentitySDK(clientId, { loginCallbackUri: '/callback', logoutCallbackUri: '/logout-callback', authority: 'https://id.vatom.com', businessId: '' })
+  const [accessToken, setAccessToken] = useState(identitySdk.getAccessToken())
 
   useEffect(() => {
-    if (divRef.current && shouldRender) {
-      new VatomWallet(divRef.current, accessToken, businessId, {
-        // baseUrl: "https://wow.vatominc.cc",
-        baseUrl: "https://wallet.localhost:3000",
-        features: {
-          // scanner: {
-          //   enabled: true,
-          // },
-        },
-        // visibleTabs: ["people", "inventory", "profile"],
-      });
+    const triggerCallback = async () => {
+      const res = await identitySdk.onCallbacks()
+      if (res) {
+        setAccessToken(identitySdk.getAccessToken())
+      }
     }
-  }, [shouldRender, accessToken, businessId]);
+    triggerCallback()
+  }, [])
+
+
+  useEffect(() => {
+    if (divRef.current && accessToken) {
+      new VatomWallet(divRef.current, accessToken.access_token, '', { baseUrl: 'https://wallet.vatom.com', features: { business: { hideHeader: true }, inventory: { hideHeader: true } } })
+    }
+  }, [divRef, accessToken])
 
   return (
-    <div>
-      {!shouldRender ? (
-        <>
-          <input
-            type="text"
-            value={accessToken}
-            placeholder="access token"
-            onChange={(e) => setAccessToken(e.target.value)}
-          />
-          <input
-            type="text"
-            value={businessId}
-            placeholder="business id"
-            onChange={(e) => setBusinessId(e.target.value)}
-          />
-          <button onClick={() => setShouldRender(true)}>Render!</button>
-        </>
-      ) : null}
-      <div ref={divRef} className="App"></div>
+    <div className="App">
+      {!accessToken ? (
+        <button onClick={identitySdk.login}>Login</button>
+      ) : (
+        <span>Logged In: </span>
+      )}
+      {accessToken && <button onClick={identitySdk.logout}>Logout</button>}
+
+      <div className="App" ref={divRef}>
+
+      </div>
     </div>
   );
 }
