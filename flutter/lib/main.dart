f943dead5c64d30e70a3e98d2e9e049e34306e29
YerimMoral3s <94862683+YerimMoral3s@users.noreply.github.com>
2023-10-18 14:42:34 -0600

    update

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -6,20 +6,20 @@ import 'package:vatom_wallet_sdk/vatom_wallet_sdk.dart';
 main() {
   final vatomKey = GlobalKey<VatomWalletState>();
   final VatomConfig vatomConfig = VatomConfig(
-    baseUrl: "https://wallet.vatominc.com",
+    baseUrl: "https://23e9-201-141-16-18.ngrok-free.app",
     features: VatomConfigFeatures(
       hideNavigation: false,
-      scanner: ScannerFeatures(enabled: false),
+      scanner: ScannerFeatures(enabled: null),
       vatom: VatomFeatures(
         hideTokenActions: true,
-        disableNewTokenToast: true,
       ),
     ),
   );
 
   final VatomWallet wallet = VatomWallet(
     key: vatomKey,
-    accessToken: "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Imw0Mjd4WnJxNjJlR0xhS0hhc0d0bkkyZ1JZVjF3c0VUUm0weDlDcEZiOWsifQ.eyJ1cm46dmF0b21pbmM6Z3Vlc3QiOmZhbHNlLCJ1cm46dmF0b21pbmM6cmVnaW9uIjoidXMtZWFzdDQuZ2NwIiwianRpIjoiaW9Ka2dtQUVzM0x1M2hMYTl2MGFXIiwic3ViIjoiMWxueWw4ZSIsImlhdCI6MTY5NzE0OTMwNSwiZXhwIjoxNjk3MTUyOTA1LCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIG9mZmxpbmVfYWNjZXNzIiwiaXNzIjoiaHR0cHM6Ly9pZC52YXRvbS5jb20iLCJhdWQiOiI5NEpIa2RqOGpGODNqZkZGMkxJOFE0In0.M7Olf--Sz5g75SO8idcMaMmZATKlAL1kQzpSVsR3SmuWRH51PloRSplXP8aT8g5RZBZb16HHJGJSHTaZe7w4Jl-C99t8p24BUUJ6JKvCRzqSeGBxOg-FoZ5vVAUvq-elvLmRyiTl36-Y2L8zf0ql314clPtG7ZuhXrXOd1iYB_wu4JfWg9W5Z0C7jtDyuphq3vDJxDfh-tgIFJFNyrbDHdfnS-2Ogrsu1sTZh2lQI7HqIoJ8QNDw6cWwlg_zkAoxfuADe0VRe4gAvpjBudOXkyFuNI8vB29AaacCRMRgCE2zHqj9Cd8tDFY0A93VyVVc9TEdPEggzq44iLc4sI90Ug",
+    accessToken:
+        "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Imw0Mjd4WnJxNjJlR0xhS0hhc0d0bkkyZ1JZVjF3c0VUUm0weDlDcEZiOWsifQ.eyJ1cm46dmF0b21pbmM6Z3Vlc3QiOmZhbHNlLCJ1cm46dmF0b21pbmM6cmVnaW9uIjoidXMtZWFzdDQuZ2NwIiwianRpIjoiRHoxRmhpdERBU2g4WG8zaEFmX1pBIiwic3ViIjoiY3hwNGs5cCIsImlhdCI6MTY5NzQ5Mjg2NywiZXhwIjoxNzAwMDg0ODY3LCJzY29wZSI6Im9wZW5pZCBlbWFpbCBwcm9maWxlIG9mZmxpbmVfYWNjZXNzIiwiaXNzIjoiaHR0cHM6Ly9pZC52YXRvbS5jb20iLCJhdWQiOiIzSDVxcHlpUXU5In0.EsPZX_kNSxQ08CkrYO3qPBZjckyfrn-rZAdahTg--LJZJJXxDHAPreyIc4KxN3wJ19AF7hy0bQrwXt3bbYKELJMbSIkIb7tduV9-0JucwB5VvvTf_fT_QfItaa1bSfep5F_JtckYOc80v2tXbKXt4At8BZHRzfw8cCCu0uB-pY3zKTr5OuWwsX9Bvjm4Et5LZzT6DDyrN7nvCGWzxaekCtNzhsH8FExWbsHWNTjZIG7IpnrxDQmjmYJHyldhcdNVjbbH4qWLddaywLuMsb9Kq6nMRzOHZzibTop5Aa3lcRgaHJZwCss1OgPOjQxLWcEUQfmmifH0AHW-8_POhvoZEw",
     config: vatomConfig,
   );
 
