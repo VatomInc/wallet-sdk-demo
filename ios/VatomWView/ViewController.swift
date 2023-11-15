561af44facb0e160fef5817a12cb7b8554037cda
Luis Palacios <lpalacios.dev@gmail.com>
2023-11-15 13:05:55 -0300

    feat: update demo to use new methods

--- ios/VatomWView/ViewController.swift
+++ ios/VatomWView/ViewController.swift
@@ -10,21 +10,231 @@ import VatomWallet
 
 class ViewController: UIViewController, UIScrollViewDelegate {
     var vatom: VatomWallet?
+    let floatingButton = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        Get the access token & refresh token after the token-exchange and pass it to the VatomWallet
-        let vatomAccessToken = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Imw0Mjd4WnJxNjJlR0xhS0hhc0d0bkkyZ1JZVjF3c0VUUm0weDlDcEZiOWsifQ.eyJ1cm46dmF0b21pbmM6bG9nZ2VkLWluLXZpYSI6Im1hZ2ljLWNvZGUiLCJ1cm46dmF0b21pbmM6Z3Vlc3QiOmZhbHNlLCJ1cm46dmF0b21pbmM6cmVnaW9uIjoidXMtZWFzdDQuZ2NwIiwianRpIjoiclQ2WWo1VmZEclVWSGhEVGozZnFCIiwic3ViIjoiajJobnN2dCIsImlhdCI6MTY3OTUwODc3NSwiZXhwIjoxNjc5NTEyMzc1LCJzY29wZSI6Im9wZW5pZCBlbWFpbCBwcm9maWxlIG9mZmxpbmVfYWNjZXNzIiwiaXNzIjoiaHR0cHM6Ly9pZC52YXRvbS5jb20iLCJhdWQiOiIzSDVxcHlpUXU5In0.kgKgh8rKODtTD4NrnC1ej8IpfVIRtJThmSNgEcllPncty4vCOxlyglgawuGz-HSfF--SbAYwpeYhveDTRwT2u7b-KSk6HNtKOIjKiQMnZJX7PbugDcTmEIomTbxiy-jnohKmLd0nHBe9iVrcILKzVBmFabbtBp871weUfkq4pckIfntFYvX9XW94xyOPYTi2CvY3gFx38c3P3cm_WN8Vw9JC4b5CKFxVZsCMBIx4VCAvEV8xfbl9eHMPg6Pn-KbHtOCRhB5aF5j_voW1hdGwXdATKTVYtgHIMQgDHa2m1wEf3Bnyo_-ExUwUP_pnwNvlFq9DvhBCHkXUbC4REx8KCQ"
-        self.vatom = VatomWallet(accessToken:vatomAccessToken,view: self.view)
+        //        Get the access token & refresh token after the token-exchange and pass it to the VatomWallet
+        let vatomAccessToken = "YOUR_ACCESS_TOKEN"
+        
+        let vatomConfigFeatures: [String: Any] = [
+            "hideNavigation": false,
+            "hideTokenActions": true,
+            "disableNewTokenToast": true,
+            "scanner": ["enabled": false],
+            "pageConfig": [
+                "theme": [
+                    "header": ["logo": "https://resources.vatom.com/a8BxS4bNj9/UR_Logo.png"],
+                    "iconTitle": [:],
+                    "icon": [:],
+                    "main": [:],
+                    "emptyState": [:],
+                    "mode": "dark",
+                    "pageTheme": "dark",
+                ],
+                "text": ["emptyState": ""],
+                "features": [
+                    "notifications": [:],
+                    "card": [:],
+                    "footer": [
+                        "enabled": true,
+                        "icons": [
+                            [
+                                "src": "https:sites.vatom.com/a8BxS4bNj9",
+                                "title": "Home",
+                                "id": "home",
+                            ],
+                        ],
+                    ],
+                    "vatom": [:],
+                ],
+            ],
+        ]
+        
+        
+        self.vatom = VatomWallet(businessId: "e2fVTm8Kuy", accessToken:vatomAccessToken,view: self.view, config: vatomConfigFeatures)
         view.addSubview(vatom!)
         
         vatom?.scrollView.delegate = self
         vatom?.scrollView.bounces = false
         vatom?.scrollView.bouncesZoom = false
-                
+        
         vatom?.load()
+        
+        // Setup the floating button
+        setupFloatingButton()
     }
-
-
+    
+    private func setupFloatingButton() {
+        // Set the button image, if you have an image asset
+        // floatingButton.setImage(UIImage(named: "YourButtonImage"), for: .normal)
+        
+        // Or set a title for the button
+        floatingButton.setTitle("U", for: .normal)
+        
+        // Style the button
+        floatingButton.backgroundColor = .systemBlue
+        floatingButton.layer.cornerRadius = 25 // Half the height to create a circular shape
+        
+        // Add targets to the button
+        floatingButton.addTarget(self, action: #selector(showActionSheet), for: .touchUpInside)
+        
+        // Disable autoresizing masks, enable autolayout
+        floatingButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Add the button to the view
+        view.addSubview(floatingButton)
+        
+        // Set constraints for the button (e.g., bottom right corner)
+        NSLayoutConstraint.activate([
+            floatingButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            floatingButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            floatingButton.widthAnchor.constraint(equalToConstant: 50),
+            floatingButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    @objc func floatingButtonTapped()   {
+        
+        
+        Task {
+            try await vatom?.navigateToTab("Connect")
+        }
+    }
+    
+    private func getUser (){
+        Task {
+            if let userData = await vatom?.getUser()  {
+                // Now that we have casted userData to a dictionary, we can access its "name" key
+                
+                // Show an alert with the user's name
+                print(userData)
+                let alertController = UIAlertController(
+                    title: "User Found",
+                    message: "Hello, \(userData.name)!",
+                    preferredStyle: .alert
+                )
+                let okAction = UIAlertAction(title: "OK", style: .default)
+                alertController.addAction(okAction)
+                present(alertController, animated: true)
+                
+            } else {
+                print("Could not retrieve user data.")
+            }
+        }
+    }
+    
+    private func isLoggedIn (){
+        Task {
+            if let isLoggedIn = await vatom?.isLoggedIn()  {
+                print("isLoggedIn",isLoggedIn)
+                // Now that we have casted userData to a dictionary, we can access its "name" key
+                // Show an alert with the user's name
+                
+                let alertController = UIAlertController(
+                    title: "Is logged in?",
+                    message: "\(isLoggedIn)!",
+                    preferredStyle: .alert
+                )
+                let okAction = UIAlertAction(title: "OK", style: .default)
+                alertController.addAction(okAction)
+                present(alertController, animated: true)
+                
+            } else {
+                print("Could not retrieve user data.")
+                
+                
+            }
+        }
+    }
+    
+    private func navigate(_ to: String) {
+        Task {
+            try await vatom?.navigateToTab(to)
+        }
+    }
+    
+    private func listTokens() {
+        Task {
+            let tokens =  try await self.vatom?.listTokens()
+            let alertController = UIAlertController(
+                title: "Tokens",
+                message: "You got, \(tokens?.count) tokens!",
+                preferredStyle: .alert
+            )
+            let okAction = UIAlertAction(title: "OK", style: .default)
+            alertController.addAction(okAction)
+            present(alertController, animated: true)
+            
+        }
+    }
+    
+    func trashFirstToken  (){
+        Task {
+            let tokens =  try await self.vatom?.listTokens()
+            if let firstToken = tokens?.first as? [String : Any?] {
+                if let id = firstToken["id"] as? String {
+                    await self.vatom?.trashToken(tokenId: id)
+                }
+                
+            }
+            
+        }
+    }
+    
+    
+    
+    
+    
+    
+    // MARK: - Action Handlers
+    @objc func showActionSheet() {
+        let actionSheet = UIAlertController(title: "Select an Option", message: nil, preferredStyle: .actionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "Navigate to Connect", style: .default, handler: { _ in
+            self.navigate("Connect")
+        }))
+        
+        
+        actionSheet.addAction(UIAlertAction(title: "Navigate to Wallet", style: .default, handler: { _ in
+            self.navigate("Wallet")
+            
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Navigate to Map", style: .default, handler: { _ in
+            self.navigate("Map")
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Navigate to Home", style: .default, handler: { _ in
+            self.navigate("Home")
+        }))
+        
+        
+        actionSheet.addAction(UIAlertAction(title: "Get User", style: .default, handler: { _ in
+            self.getUser()
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "How many tokens?", style: .default, handler: { _ in
+            self.listTokens()
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "isLoggedIn?", style: .default, handler: { _ in
+            self.isLoggedIn()
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Trash first token", style: .default, handler: { _ in
+            self.trashFirstToken()
+        }))
+        
+        
+        
+        // Cancel action
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        self.present(actionSheet, animated: true)
+    }
+    
 }
+
 
