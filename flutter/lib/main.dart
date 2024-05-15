aa75729867186dad5eb64c125e4baa189113fc28
YerimMoral3s <94862683+YerimMoral3s@users.noreply.github.com>
2024-05-15 12:33:06 -0600

    update

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,27 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:vatom_wallet_sdk/vatom_wallet_sdk.dart';
 
-// import 'package:my_app/gradient_color.dart';
-
 main() {
-  final vatomKey = GlobalKey<VatomWalletState>();
-  final VatomConfig vatomConfig = VatomConfig(
-    baseUrl: "https://23e9-201-141-16-18.ngrok-free.app",
-    features: VatomConfigFeatures(
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final VatomWallet wallet = VatomWallet(
+    accessToken: "...accessToken",
+    refreshToken: "...refreshToken",
+    initialRoute: "map",
+    config: VatomConfigFeatures(
+      hideTokenActions: true,
+      disableArPickup: true,
+      disableNewTokenToast: true,
+      hideDrawer: false,
       hideNavigation: false,
-      scanner: ScannerFeatures(enabled: null),
-      vatom: VatomFeatures(
-        hideTokenActions: true,
+      language: "en",
+      scanner: ScannerFeatures(enabled: false),
+      pageConfig: PageConfig(
+        features: PageFeatures(
+          icon: PageFeaturesIcon(badges: false, editions: false, titles: false),
+          footer: PageFeaturesFooter(enabled: true, icons: [
+            PageFeaturesFooterIcon(id: "map", src: "", title: "Map"),
+          ]),
+        ),
       ),
     ),
   );
 
-  final VatomWallet wallet = VatomWallet(
-    key: vatomKey,
-    accessToken:
-        "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Imw0Mjd4WnJxNjJlR0xhS0hhc0d0bkkyZ1JZVjF3c0VUUm0weDlDcEZiOWsifQ.eyJ1cm46dmF0b21pbmM6Z3Vlc3QiOmZhbHNlLCJ1cm46dmF0b21pbmM6cmVnaW9uIjoidXMtZWFzdDQuZ2NwIiwianRpIjoiRHoxRmhpdERBU2g4WG8zaEFmX1pBIiwic3ViIjoiY3hwNGs5cCIsImlhdCI6MTY5NzQ5Mjg2NywiZXhwIjoxNzAwMDg0ODY3LCJzY29wZSI6Im9wZW5pZCBlbWFpbCBwcm9maWxlIG9mZmxpbmVfYWNjZXNzIiwiaXNzIjoiaHR0cHM6Ly9pZC52YXRvbS5jb20iLCJhdWQiOiIzSDVxcHlpUXU5In0.EsPZX_kNSxQ08CkrYO3qPBZjckyfrn-rZAdahTg--LJZJJXxDHAPreyIc4KxN3wJ19AF7hy0bQrwXt3bbYKELJMbSIkIb7tduV9-0JucwB5VvvTf_fT_QfItaa1bSfep5F_JtckYOc80v2tXbKXt4At8BZHRzfw8cCCu0uB-pY3zKTr5OuWwsX9Bvjm4Et5LZzT6DDyrN7nvCGWzxaekCtNzhsH8FExWbsHWNTjZIG7IpnrxDQmjmYJHyldhcdNVjbbH4qWLddaywLuMsb9Kq6nMRzOHZzibTop5Aa3lcRgaHJZwCss1OgPOjQxLWcEUQfmmifH0AHW-8_POhvoZEw",
-    config: vatomConfig,
-  );
+  void linkTo(String path) async {
+    wallet.linkTo(path).catchError(
+          (error) => print('Error: $error'),
+        );
+  }
 
   runApp(
     MaterialApp(
@@ -34,27 +44,42 @@ main() {
               ),
               Row(
                 children: [
-                  ElevatedButton(
-                    onPressed: () async {
-                      // await vatomKey.currentState?.openCommunity('sahMOa1qQR',
-                      //     roomId: '!EcINyJyITqdmqVEAVe%3Avatom.com');
-                      var tabs = await vatomKey.currentState?.getCurrentUser();
-                      print(tabs?.toJson());
-                    },
-                    child: Text('getCurrentUser'),
+                  Padding(
+                      padding: EdgeInsets.all(3),
+                      child: ElevatedButton(
+                        onPressed: () async {
+                          var tabs = await wallet.getCurrentUser();
+                          print(tabs?.toJson());
+                        },
+                        child: Text('getCurrentUser'),
+                        style: ElevatedButton.styleFrom(
+                          textStyle: const TextStyle(fontSize: 12),
+                        ),
+                      )),
+                  Padding(
+                    padding: EdgeInsets.all(3),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        linkTo("/map");
+                      },
+                      child: Text('map'),
+                      style: ElevatedButton.styleFrom(
+                        textStyle: const TextStyle(fontSize: 12),
+                      ),
+                    ),
                   ),
-                  ElevatedButton(
-                    onPressed: () async {
-                      await vatomKey.currentState?.navigateToTab("Connect");
-                    },
-                    child: Text('Connect'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () async {
-                      await vatomKey.currentState?.navigateToTab("Home");
-                    },
-                    child: Text('Home'),
-                  ),
+                  Padding(
+                      padding: EdgeInsets.all(3),
+                      child: ElevatedButton(
+                        onPressed: () async {
+                          await wallet.navigateToTab("Wallet");
+                        },
+                        child: const Text('wallet (deprecated)'),
+                        //break text to avoid overflow
+                        style: ElevatedButton.styleFrom(
+                          textStyle: const TextStyle(fontSize: 12),
+                        ),
+                      )),
                 ],
               ),
             ],
@@ -64,5 +89,3 @@ main() {
     ),
   );
 }
-// export 'package:vatom_wallet_sdk/VatomClasses.dart';
-// export 'package:vatom_wallet_sdk/VatomWallet.dart' show VatomWallet;
