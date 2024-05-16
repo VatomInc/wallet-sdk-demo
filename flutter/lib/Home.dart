3e024271c0e5a99875d5333d503f58ba57fb78a2
YerimMoral3s <94862683+YerimMoral3s@users.noreply.github.com>
2024-05-15 18:10:30 -0600

    add sheet

--- flutter/lib/Home.dart
+++ flutter/lib/Home.dart
@@ -7,9 +7,81 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('this is the home page'),
+        child: BottomSheetExample(
+          walletController: walletController,
+        ),
+      ),
+    );
+  }
+}
+
+class BottomSheetExample extends StatelessWidget {
+  final VatomWalletController? walletController;
+  const BottomSheetExample({super.key, required this.walletController});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        child: const Text('Open AR'),
+        onPressed: () {
+          walletController?.linkTo("/ar");
+          showModalBottomSheet<void>(
+            context: context,
+            isScrollControlled: true,
+            builder: (BuildContext context) {
+              return FractionallySizedBox(
+                heightFactor: 0.9,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton(
+                        child: const Text('Close BottomSheet'),
+                        onPressed: () {
+                          walletController?.linkTo("/");
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ),
+                    Expanded(
+                      child: _Ar(
+                        walletController: walletController,
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _Ar extends StatelessWidget {
+  final VatomWalletController? walletController;
+  const _Ar({super.key, required this.walletController});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SizedBox(
+          width: 400, // Ancho deseado
+          height: 600, // Alto deseado
+          child: Stack(
+            children: [
+              walletController?.wallet ??
+                  const Center(child: CircularProgressIndicator()),
+            ],
+          ),
+        ),
       ),
     );
   }
